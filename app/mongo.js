643215e0f659fc4b8f4b87b5f60e4411b81abfde
Njoroge Mwangi <ufalmeart@gmail.com>
2024-02-12 17:46:22 +0300

    added mongo db

--- app/mongo.js
+++ app/mongo.js
@@ -1,7 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(process.env.MONGODB_URI ?? '', {
+const client = new MongoClient(process.env.MONGODB_URI, {
     serverApi: {
       version: ServerApiVersion.v1,
       strict: true,
